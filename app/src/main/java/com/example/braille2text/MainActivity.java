1d156998ee7d3b2b0ece3608f0f7d69821b3b76b
Patrick Friedman <patrick.s.friedman@gmail.com>
2020-10-06 16:51:09 -0500

    OpenCV Installed

---
+++ app/src/main/java/com/example/braille2text/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.braille2text;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity
+{
+    private static String TAG = "MainActivity";
+
+    static
+    {
+        if (OpenCVLoader.initDebug())
+        {
+            Log.d(TAG, "OpenCV is Configured or Connected Successfully.");
+        }
+        else
+        {
+            Log.d(TAG, "Failed.");
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
