3cdf6f46f7316dcec441bde468173daa7768859a
Michael Rivera <michaelrivera3@my.unt.edu>
2020-10-13 16:25:57 -0500

    new activity added called "CameraActivity"

---
+++ app/src/main/java/com/example/braille2text/CameraActivity.java
@@ -0,0 +1,14 @@
+package com.example.braille2text;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+}
